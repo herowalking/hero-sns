e0cf4c5c17a66e6f1d694669d6d9777043d9f6c4
Lincoln <lincolnpb@hotmail.com>
2017-06-19 10:58:46 +0800

    调整首页左右部分所占宽度

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -2,14 +2,14 @@
   <div id="app">
     <!--<div v-for="route in $router.options.routes">{{route.name}}</div>-->
     <el-row>
-      <el-col :md="4">
+      <el-col :md="3">
         <el-menu>
           <el-menu-item v-for="route in $router.options.routes" :index="route.path" @click="click(route.path)">
             {{route.name}}
           </el-menu-item>
         </el-menu>
       </el-col>
-      <el-col :md="20">
+      <el-col :md="19" :push="1">
         <router-view></router-view>
       </el-col>
     </el-row>
